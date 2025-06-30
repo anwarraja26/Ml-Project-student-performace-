4cc1964e6cdcf08ef3ddf22fa2157dc6956d7dce
anwarraja-dotcom <anwarraja70884580@gmail.com>
2025-06-30 17:49:06 +0530

    model_trainer added

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ import sys
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score,accuracy_score, f1_score, precision_score, recall_score
+from sklearn.model_selection import GridSearchCV
+
 def save_object(file_path,obj):
     try:
         dir_path=os.path.dirname(file_path)
@@ -16,10 +18,16 @@ def save_object(file_path,obj):
     
 from sklearn.metrics import r2_score
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,param):
     try:
         report = {}
         for model_name, model in models.items():
+            params=param[model_name]
+
+            gs = GridSearchCV(model,params,cv=2)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
